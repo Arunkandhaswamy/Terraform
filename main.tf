bc7968d1e5aec5bbf2fdbf8b8d5a50e882186f86
Arunkandhaswamy <arunkumark104619@gmail.com>
2025-01-17 14:25:27 +0530

    update

--- main.tf
+++ main.tf
@@ -1,18 +1,5 @@
 provider "aws" {
     region = "us-west-2"
-}
-
-resource "aws_instance" "example" {
-    ami           = "ami-0c55b159cbfafe1f0" # Replace with a valid AMI ID for your region
-    instance_type = "t2.micro"
-    
-    tags = {
-        Name = "example-instance"
-    }
-}
-
-provider "aws" {
-    region = "us-est-2"
 
 }
 resource "aws_instance" "example2" {
