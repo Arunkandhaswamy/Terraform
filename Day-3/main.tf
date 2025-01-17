bdd28fe67ddd7b795413244cba5f5bccb5e578e7
Arunkandhaswamy <arunkumark104619@gmail.com>
2025-01-17 19:45:09 +0530

    updated

--- Day-3/main.tf
+++ Day-3/main.tf
@@ -1,17 +1,3 @@
-variable "ami_id" {
-    description = "The AMI ID to use for the instance"
-    type = string
-}
-variable "instance_type" {
-    description = "The type of instance to launch"
-    type = string
-}
-
-
-
-provider "aws" {
-  region = "us-west-2"
-}
 
 resource "aws_instance" "arun_kumar" {
   ami           = "var.ami_id"
