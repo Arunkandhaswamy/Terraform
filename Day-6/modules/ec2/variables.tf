e5ccc106d45d745f726d3cb54726f2599c619624
Arunkandhaswamy <arunkumark104619@gmail.com>
2025-01-19 22:39:52 +0530

    test

--- Day-6/modules/ec2/variables.tf
+++ Day-6/modules/ec2/variables.tf
@@ -5,10 +5,13 @@ variable "cidr" {
 
 
 variable "subnet1"   {
-  default = "10.0.1.0/24"}
+  cidr_block= "10.0.1.0/24"
+  }
 
 variable "instance_type" {
-  default = "t2.micro"} 
+  default = "t2.micro"
+  } 
 
 variable "aws-ami" {
-  default = "ami-0c55b159cbfafe1f0"}    
+  default = "ami-0c55b159cbfafe1f0"
+  }    
