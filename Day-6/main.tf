aa7884697eb8b4a4af21aaa3878c179a5678f6c3
Arunkandhaswamy <arunkumark104619@gmail.com>
2025-01-19 22:57:26 +0530

    gg

--- Day-6/main.tf
+++ Day-6/main.tf
@@ -4,8 +4,8 @@ provider "aws" {
 
 module "ec2" {
     source = "./modules/ec2"
-    aws_subnet_id = aws_subnet.default.id
-    aws_instance_type = "t3.micro"
+  subnet_id= aws_subnet.default.id
+    instance_type = "t3.micro"
 
     tags = {
         Name = "ec2-instance2"
