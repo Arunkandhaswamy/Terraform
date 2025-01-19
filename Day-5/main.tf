32dd22690f1eb6793a52db65502bdb89805d258e
Arunkandhaswamy <arunkumark104619@gmail.com>
2025-01-19 18:51:15 +0530

    test

--- Day-5/main.tf
+++ Day-5/main.tf
@@ -29,7 +29,7 @@ resource "aws_route_table" "my_route_table" {
   vpc_id = aws_vpc.my_vpc.id
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = "aws_internet_gateway.my_igw.id"
+    gateway_id = aws_internet_gateway.my_igw.id
   }
 }
 
@@ -40,7 +40,7 @@ resource "aws_route_table_association" "my_subnet_association" {
 
 resource "aws_security_group" "my_sg" {
 name      = "web"
-vpc_id = "${aws_vpc.my_vpc.id}"
+vpc_id = aws_vpc.my_vpc.id
 
 ingress {
     description = "Allow HTTP from VPC"
