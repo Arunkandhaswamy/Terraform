c481c68f173ec15ba27bdb47e13644fcc6925374
Arunkandhaswamy <arunkumark104619@gmail.com>
2025-01-19 22:36:39 +0530

    tets

--- Day-6/modules/ec2/variables.tf
+++ Day-6/modules/ec2/variables.tf
@@ -1,11 +1,14 @@
-var "cidr"  {
-  default = "10.0.0.0/16"}
+variable "cidr" {   
+  type        = string
+  default     = "10.0.0.0/16"  
+}
 
-var "subnet1"   {
+
+variable "subnet1"   {
   default = "10.0.1.0/24"}
 
-var "instance_type" {
+variable "instance_type" {
   default = "t2.micro"} 
 
-var "aws-ami" {
+variable "aws-ami" {
   default = "ami-0c55b159cbfafe1f0"}    
