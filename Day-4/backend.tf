3787989edff72d048492ccfb5ec98a1220a6f45b
Arunkandhaswamy <arunkumark104619@gmail.com>
2025-01-19 10:02:41 +0530

    test

--- Day-4/backend.tf
+++ Day-4/backend.tf
@@ -3,7 +3,7 @@ terraform {
      bucket= "arun-demo-test-bucket-1"
      region = "us-west-2"
      key = "terraform.tfstate"
-    aws_dynamodb_table = "test_table"
+     aws_dynamodb_table = "test_table"
   }
 
 
