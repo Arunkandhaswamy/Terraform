13d5bc59b0ecf5ceb09a486e33b1af34c3109aa7
Arunkandhaswamy <arunkumark104619@gmail.com>
2025-01-19 22:34:08 +0530

    test

--- Day-6/modules/ec2/main.tf
+++ Day-6/modules/ec2/main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-west-2"
 }
 resource "aws_vpc" "main" {
-  cidr_block = "var.cidr"
+  cidr_block = var.cidr
 
     tags = {
         Name = "main"
@@ -13,10 +13,10 @@ resource "aws_subnet" "subnet1"{
 
 vpc_id= aws_vpc.main.id
 
-cidr_block =" var.system_subnet_cidr"
+cidr_block =var.subnet1
 }
 resource "aws_instance" "example"{
-    ami = "ami-0c55b159cbfafe1f0"
-    instance_type = "t2.micro"
+    ami = var.ami_id
+    instance_type = var.instance_type
     sudnet_id = aws_subnet.subnet1.id
     }
---
+++ Day-6/modules/ec2/variables.tf
@@ -0,0 +1,11 @@
+var "cidr"  {
+  default = "10.0.0.0/16"}
+
+var "subnet1"   {
+  default = "10.0.1.0/24"}
+
+var "instance_type" {
+  default = "t2.micro"} 
+
+var "aws-ami" {
+  default = "ami-0c55b159cbfafe1f0"}    
