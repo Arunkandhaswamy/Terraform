ecb2923ad3b24012e526af6eed1fd767fead9cda
Arunkandhaswamy <arunkumark104619@gmail.com>
2025-01-19 22:59:58 +0530

    ff

--- Day-6/main.tf
+++ Day-6/main.tf
@@ -4,10 +4,7 @@ provider "aws" {
 
 module "ec2" {
     source = "./modules/ec2"
-  subnet_id= aws_subnet.default.id
     instance_type = "t3.micro"
 
-    tags = {
-        Name = "ec2-instance2"
-    }
+   
 }
