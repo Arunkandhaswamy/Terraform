2bc9a0ce3f27008ea461fcddaa30b91b65a8fd27
Arunkandhaswamy <arunkumark104619@gmail.com>
2025-01-19 23:14:06 +0530

    ff

--- Day-6/modules/ec2/main.tf
+++ Day-6/modules/ec2/main.tf
@@ -19,5 +19,5 @@ cidr_block ="10.0.1.0/24"
 resource "aws_instance" "example"{
     ami = var.ami_id
     instance_type = var.instance_type
-    subnet_id   = aws_subnet.subnet1
+    subnet_id   = aws_subnet.subnet1.id
     }
